0ed7f2cfdfbda0689a67dbcc837e4311f9c95c93
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2024-01-21 13:42:38 +0600

    AddFoodView UI Create

---
+++ CaloCompass/Views/AddFoodView.swift
@@ -0,0 +1,43 @@
+//
+//  AddFoodView.swift
+//  CaloCompass
+//
+//  Created by Mahmudul Hasan on 21/1/24.
+//
+
+import SwiftUI
+
+struct AddFoodView: View {
+    
+    @Environment(\.managedObjectContext) var managedObject
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var calories: Double = 0
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Food name", text: $name)
+                VStack{
+                    Text("Calories : \(Int(calories))")
+                    Slider(value: $calories, in: 0...1000, step: 10)
+                } .padding()
+                
+                HStack {
+                    Spacer()
+                    Button("Submit") {
+                        DataController().addFood(name: name, calories: calories, context: managedObject)
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct AddFoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFoodView()
+    }
+}
