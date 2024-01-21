69d7e1cde8e05cc79c91c4cec6c0258f3c3106da
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2024-01-21 13:24:42 +0600

    editFood function create

--- CaloCompass/DataModel/DataController.swift
+++ CaloCompass/DataModel/DataController.swift
@@ -40,5 +40,10 @@ class DataController: ObservableObject {
         save(context: context)
     }
     
-
+    func editFood(food: Food, name: String, calories: Double, context: NSManagedObjectContext) {
+        
+        food.date = Date()
+        food.name = name
+        food.calories = calories
+    }
 }
