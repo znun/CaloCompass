0bf8112cd3dc77e8607262b1c8e500fd7a8931f9
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2024-01-21 17:45:23 +0600

    Home UI view

--- CaloCompass/Views/ContentView.swift
+++ CaloCompass/Views/ContentView.swift
@@ -6,17 +6,71 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+
+    @Environment(\.managedObjectContext) var managedObjContent
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var food: FetchedResults<Food>
+    @State private var showingAddView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("\(Int(totalCaloriesToday())) Kcal (today)")
+                    .foregroundColor(.gray)
+                    .padding(.horizontal)
+                
+                List {
+                    ForEach(food) { food in
+                        NavigationLink(destination: Text("\(food.calories)")) {
+                            HStack{
+                            VStack(alignment: .leading, spacing: 6) {
+                                Text(food.name!)
+                                    .bold()
+                                Text("\(Int(food.calories))") + Text(" calories").foregroundColor(.red)
+                                
+                            }
+                            Spacer()
+                            Text(calcTimeSince(date: food.date!))
+                                    .foregroundColor(.gray)
+                                    .italic()
+                        }
+                    }
+                }
+                    .onDelete(perform: deleteFood)
+                    
+                }
+                .listStyle(.plain)
+            }
+            .navigationTitle("CaloCompass")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button{
+                        showingAddView.toggle()
+                    } label: {
+                      Label("Add Food", systemImage: "plus.circle")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
+            .sheet(isPresented: $showingAddView){
+                AddFoodView()
+            } .navigationViewStyle(.stack)
         }
-        .padding()
     }
+    
+    private func deleteFood(offSets: IndexSet) {
+        //pass
+        
+    }
+    
+    private func totalCaloriesToday() -> Double {
+        return 0.00
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
